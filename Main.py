63e4915d4031cba0d30d5ea1692aa0d252f14dbc
MH2033 <mh.akbare@gmail.com>
2019-06-04 10:34:44 +0430

    Printing main menu and disabling CTRL+C for exit

--- Main.py
+++ Main.py
@@ -2,4 +2,20 @@ import Add
 import Delete
 import Edit
 import Report
+from os import system
 
+def main():
+    while True:
+        system("cls")
+        print("1.Add")
+        print("2.Delete")
+        print("3.Edit")
+        print("4.Report")
+        print("5.Exit")
+        x = input()
+try:
+    main()
+except KeyboardInterrupt:
+    main()
+finally:
+    main()
